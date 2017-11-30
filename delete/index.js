7773d96c0fea63a37f51e43b6705d6da0be4f86f
Nastassia <nastassia.rashid@gmail.com>
2017-11-30 15:35:45 -0800

    Delete

--- delete/index.js
+++ delete/index.js
@@ -1,20 +1,27 @@
-const mongoose = require('mongoose');
-const _ = require('lodash');
-const Q = require('q');
+var DocumentDBClient = require('documentdb').DocumentClient;
+
+var host = process.env[CosmosDB_endpoint];
+var masterKey = process.env[ComosDB_authKey];
+var databaseId = process.env[CosmosDB_databaseId];
+var collectionId = process.env[CosmosDB_collectionId];
+
+// Establish a new instance of the DocumentDBClient to be used throughout this demo
+var client = new DocumentDBClient( host, { masterKey: masterKey });
 
 module.exports = function(context, req)
 {
-    GameServer.deleteThing(context.bindingData.server)
-        .then(() => {
-            return context.res = {
-                status: 200
-            };
-        })
-        .catch((err) => {
-            context.log(err, err.stack);
+    var docLink = 'dbs/' + databaseId + '/colls/' + collectionId + '/docs/' + docId;
 
-            return context.res = {
+    client.deleteDocument(docLink, function (err) {
+        if (err) {
+            context.res = {
                 status: 500
             };
-        });
+        } else {
+            context.res = {
+                status: 200
+            };
+        }
+        context.done();
+    });
 }

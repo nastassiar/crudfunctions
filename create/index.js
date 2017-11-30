6f48af5e9ad2b69bbf483a8942171dc007634267
Jef King <jefkin@microsoft.com>
2017-11-30 09:48:19 -0800

    CRUD/HTTP

--- create/index.js
+++ create/index.js
@@ -1,15 +1,15 @@
 exports.create = (req, res, next) => {
-    
+
     const data = req.body;
     thing.insertThing(data)
-    .then((server) => {
-        return res.json({
-            status: true,
-            message: server
+        .then((server) => {
+            return res.json({
+                status: true,
+                message: server
+            });
+        })
+        .catch((err) => {
+            console.log(err, err.stack);
+            next(err);
         });
-    })
-    .catch((err) => {
-        console.log(err, err.stack);        
-        next(err);
-    });
 };

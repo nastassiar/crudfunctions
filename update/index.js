2f199f8e08654611f3cfed23a77ba102cee8ebd6
Nastassia <nastassia.rashid@gmail.com>
2017-11-30 10:48:39 -0800

    Update?
    First crack at update

--- update/index.js
+++ update/index.js
@@ -1,16 +1,27 @@
-exports.update = (req, res, next) => {
-    const data = req.body;
-    const thingId = req.params.thing;
-
-    Thing.update(thingId, data)
-    .then((server) => {
-        return res.json({
-            status: true,
-            message: server
-        });
-    })
-    .catch((err) => {
-        console.log(err, err.stack);
-        next(err);
-    });
+module.exports = function (context, req) {    
+    var documents = context.bindings.documents;
+    if (documents.length == 1 && req.body) {
+        
+        var document = documents[0];
+        req.body.id = context.bindings.id;
+        context.bindings.document = req.body;
+        
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: req.body
+        };
+    }
+    else if (documents.length == 0) {
+        context.res = {
+            status: 404, 
+            body: "Document with id "+context.bindings.id+" not found"
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Error"
+        };
+    }       
+    context.done();
 };

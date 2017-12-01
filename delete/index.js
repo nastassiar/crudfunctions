a68a863377fe14c94d49227fc635177b2abc8f99
Nastassia <nastassia.rashid@gmail.com>
2017-11-30 19:46:59 -0800

    Testing
    Updating delete to have item as input to make sure it exists before deleting

--- delete/index.js
+++ delete/index.js
@@ -1,27 +1,39 @@
 var DocumentDBClient = require('documentdb').DocumentClient;
 
-var host = process.env[CosmosDB_endpoint];
-var masterKey = process.env[ComosDB_authKey];
-var databaseId = process.env[DatabaseName];
-var collectionId = process.env[CollectionName];
+var host = process.env["CosmosDBEndpoint"];
+var masterKey = process.env["CosmosDBAuthKey"];
+var databaseId = process.env["DatabaseName"];
+var collectionId = process.env["CollectionName"];
 
-// Establish a new instance of the DocumentDBClient to be used throughout this demo
 var client = new DocumentDBClient( host, { masterKey: masterKey });
+// Establish a new instance of the DocumentDBClient to be used throughout 
+
 
 module.exports = function(context, req)
 {
-    var docLink = 'dbs/' + databaseId + '/colls/' + collectionId + '/docs/' + docId;
-
-    client.deleteDocument(docLink, function (err) {
-        if (err) {
-            context.res = {
-                status: 500
-            };
-        } else {
-            context.res = {
-                status: 200
-            };
-        }
+   console.log(masterKey);
+    var docLink = 'dbs/' + databaseId + '/colls/' + collectionId + '/docs/' + context.bindingData.id;
+    console.log(docLink);
+    if (documents.length == 0) {
+        context.res = {
+            status: 404, 
+            body: "Document with id "+context.bindings.id+" not found"
+        };
         context.done();
-    });
+    }
+    else {     
+        client.deleteDocument(docLink, function (err) {
+            if (err) {
+                context.res = {
+                    status: 500,
+                    message: error
+                };
+            } else {
+                context.res = {
+                    status: 200
+                };
+            }
+            context.done();
+        });
+    }
 }

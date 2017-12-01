da204e7e321073e8261da38b8452a17b76ce36df
Nastassia <nastassia.rashid@gmail.com>
2017-11-30 17:09:08 -0800

    Add an id to create

--- create/index.js
+++ create/index.js
@@ -1,9 +1,11 @@
 module.exports = function (context, req) {    
-    if (req.body) {
-    //if (req.body && req.body.name) {
-
+    if (req.body) 
+    {
+        if (req.body.id == undefined || req.body.id == null)
+        {
+            req.body.id = Math.random().toString(26).slice(2)
+        }
         context.bindings.document = req.body;
-        // Set an id?
         context.res = {
             status: 200, /* Defaults to 200 */
             body: req.body
